a8f54072634405b034cb1238fecaed41e47a46b6
anshooool <140979513+anshooool@users.noreply.github.com>
2023-08-31 02:02:12 +0530

    Create OSXpersistence.sh

---
+++ OSXpersistence.sh
@@ -0,0 +1,17 @@
+
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>Label</key>
+    <string>com.example.exampled</string>
+    <key>LaunchOnlyOnce</key>
+        <true/>
+    <key>ProgramArguments</key>
+    <array>
+        <string>/bin/bash</string>
+        <string>{b}/systemfaster.sh</string>
+
+    </array>
+</dict>
+</plist>
